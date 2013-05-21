355c2c9945f362ec910b9f55f498f133cc382232
yangiYA <dummy@dummy.co.jp>
2013-05-21 12:30:58 +0900

    refactoring

--- src/main/scala/textimg/TextImageServlet.scala
+++ src/main/scala/textimg/TextImageServlet.scala
@@ -6,58 +6,41 @@ import javax.servlet.ServletOutputStream
 
 class TextImageServlet extends TextimgStack {
 
-  val html: scala.xml.Elem = <html>
-                               <body>
-                                 <h1>Hello, world!</h1>
-                                 Say<a href="hello-scalate">hello to Scalate { """hogehoge""" }</a>
-                                 .
-                               </body>
-                             </html>
-  val aaaa = """\d+"""
-  /*
-  get("/") {
+  val notFoundHtml =
     <html>
-      <body>
-        <h1>Hello, world!</h1>
-        Say<a href="hello-scalate">hello to Scalate</a>
-        .
-      </body>
+      <body>Page not found !</body>
     </html>
-  }
- */
 
-  get("/*") { NotFound("""It's could not be found""") }
+  get("/*")(NotFound(notFoundHtml))
+
   get("/img", params.getOrElse("str", "").length != 0) {
-    val imgType = "png"
-    contentType = "image/" + imgType
-    Text2Image.makeImageByteArray(params.get("str"), None, None, extension = imgType)
+    imageResult(params.get("str"), "png")
   }
 
   get("/img.:extension", params.getOrElse("str", "").length != 0) {
-    val imgType = params("extension")
-    val imgExtList: List[String] = List("png", "jpg", "jpeg", "gif")
-    if (imgExtList.exists { _.equalsIgnoreCase(imgType) }) {
-      contentType = "image/" + imgType
-      Text2Image.makeImageByteArray(params.get("str"), None, None, extension = imgType)
-    } else {
-      NotFound("""It's could not be found!""")
-    }
+    imageResult(params.get("str"), params("extension"))
   }
 
   get("/img/:extension", params.getOrElse("str", "").length != 0) {
-    val imgType = params("extension")
+    imageResult(params.get("str"), params("extension"))
+  }
+
+  get("/textimg/:string/img.:extension") {
+    imageResult(Some(params("string")), params("extension"))
+  }
+
+  private def imageByteArrayActionResult(string: Option[String], imageType: String) = {
+    contentType = "image/" + imageType.toLowerCase()
+    Text2Image.makeImageByteArray(string, None, None, extension = imageType)
+  }
+
+  private def imageResult(string: Option[String], imageType: String) = {
     val imgExtList: List[String] = List("png", "jpg", "jpeg", "gif")
-    if (imgExtList.exists { _.equalsIgnoreCase(imgType) }) {
-      contentType = "image/" + imgType
-      Text2Image.makeImageByteArray(params.get("str"), None, None, extension = imgType)
+    if (imgExtList exists (_ equalsIgnoreCase imageType)) {
+      imageByteArrayActionResult(string, imageType)
     } else {
-      NotFound("""It's could not be found!""")
+      NotFound( """It's could not be found!""")
     }
   }
 
-  //  get("/img.png", params.getOrElse("str", "").length != 0) {
-  //    val imgType = "png"
-  //    contentType = "image/" + imgType
-  //    Text2Image.makeImageByteArray(params.get("str") + "", None, None, extension = imgType)
-  //  }
 }
